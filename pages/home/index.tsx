fd951e997e996b706ed8845faa57a7ffa7a25ca4
hwmin414 <hwmin414@kiast.ac.kr>
2023-05-19 17:25:34 +0900

    feat: wallet balance check && antd

---
+++ pages/home/index.tsx
@@ -0,0 +1,25 @@
+import { useEvmNativeBalance } from '@moralisweb3/next';
+import { Input, Space } from 'antd';
+import { useState } from 'react';
+
+function getBalance(props) {
+    const {address} = props;
+    const { data: nativeBalance } = useEvmNativeBalance({ address });
+    return (
+        <h3>Native Balance: {nativeBalance?.balance?.ether} ETH</h3>
+    )
+}
+
+export default function Home() {
+    const [address, setAddress] = useState<string>('');
+
+    return (
+        <div>
+            <Space.Compact style={{ width: '100%' }}>
+                <Input placeholder="MetaMask Address" onChange={(evt) => {setAddress(evt.target.value)}} />
+            </Space.Compact>
+            <h3>Wallet: {address}</h3>
+            {getBalance({address})}
+        </div>
+    );
+}
