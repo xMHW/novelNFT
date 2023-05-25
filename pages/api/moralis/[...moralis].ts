7155f7bd0d6d8f1e6bf0daa06b773397d96ea026
Minseong <hwmin414@kaist.ac.kr>
2023-05-25 13:23:44 +0900

    feat: add metamask login (#4)
    Co-authored-by: hwmin414 <hwmin414@kiast.ac.kr>

--- pages/api/moralis/[...moralis].ts
+++ pages/api/moralis/[...moralis].ts
@@ -1,3 +1,10 @@
 import { MoralisNextApi } from "@moralisweb3/next";
 
-export default MoralisNextApi({ apiKey: process.env.MORALIS_API_KEY });
+export default MoralisNextApi({
+  apiKey: process.env.MORALIS_API_KEY,
+  authentication: {
+    domain: "novel-nft.vercel.app",
+    uri: process.env.NEXTAUTH_URL,
+    timeout: 120,
+  },
+});
